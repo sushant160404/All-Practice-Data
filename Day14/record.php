11256e3581f1776150221db3e8515a2fe41e1e0a
SUSHANT RAVINDRA NAMURTE <126276447+sushant160404@users.noreply.github.com>
2024-06-02 23:05:21 +0530

    Add files via upload
    nice web

--- Day14/record.php
+++ Day14/record.php
@@ -12,8 +12,8 @@ $password=$_POST["password"];
 $data="INSERT INTO login VALUES('','$uname','$password')";
 $check=mysqli_query($conn,$data);
 if($check){
-    //echo "Data Sended Sushant!";
-    header('Location: http://127.0.0.1:5500/about/index.html');
+    echo "Data Sended Sushant!";
+    //header('Location: http://127.0.0.1:5500/about/index.html');
 }else{
     echo "Data is not send!";
 }
