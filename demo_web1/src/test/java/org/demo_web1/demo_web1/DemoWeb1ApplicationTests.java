96118667cd902df632a27426d950c59e14658f41
SUSHANT RAVINDRA NAMURTE <126276447+sushant160404@users.noreply.github.com>
2024-07-22 22:43:35 +0530

    nice...

---
+++ demo_web1/src/test/java/org/demo_web1/demo_web1/DemoWeb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo_web1.demo_web1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWeb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
