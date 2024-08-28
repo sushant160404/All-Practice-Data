19cba09276bad73dac57d6b83186a870e7e1c006
SUSHANT RAVINDRA NAMURTE <126276447+sushant160404@users.noreply.github.com>
2024-08-28 23:52:58 +0530

    nice try

---
+++ demo (1)/demo/src/main/java/com/WebExample/demo/WebExampleApplication.java
@@ -0,0 +1,13 @@
+package com.WebExample.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebExampleApplication.class, args);
+	}
+
+}
---
+++ demo (1)/demo/src/test/java/com/WebExample/demo/WebExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WebExample.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
