6263df35649cf318e1b85d2f9cf251b38581817c
SUSHANT RAVINDRA NAMURTE <126276447+sushant160404@users.noreply.github.com>
2024-09-22 23:05:06 +0530

    Add files via upload
    practice1

---
+++ demo_web1/src/main/java/org/demo_web1/demo_web1/DemoWeb1Application.java
@@ -0,0 +1,14 @@
+package org.demo_web1.demo_web1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWeb1Application {
+
+	public static void main(String[] args) {
+		System.out.println("Sushant Demo");
+		//SpringApplication.run(DemoWeb1Application.class, args);
+	}
+
+}
